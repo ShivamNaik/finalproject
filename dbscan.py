cef2ed8a0320c19aaa14fd898c2efb91a9331446
Shivam Naik <shivam.naik@gmail.com>
2017-02-27 20:29:28 -0800

    added comparison to DB scan, still have to plot function for db scan

--- dbscan.py
+++ dbscan.py
@@ -1,7 +1,13 @@
+from sklearn.cluster import DBSCAN
 
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+from sklearn.cluster import DBSCAN
+from sklearn import metrics
+from sklearn.datasets.samples_generator import make_blobs
+from sklearn.preprocessing import StandardScaler
+
 
 NOTVISITED = -2
 NOISE = -1
@@ -12,7 +18,6 @@ class dbscan:
         sum = 0
         for i in xrange(len(point_a)):
             sum += (math.pow(point_a[i]-point_b[i], 2))
-            print (math.pow(point_a[i]-point_b[i], 2))
         return math.sqrt(sum)
 
     def regionQuery(self, dataMatrix, pointIndex, eps):
@@ -52,11 +57,40 @@ class dbscan:
 
 
 def test_dbscan():
-    m = [[1, 1.1], [1.2, .8], [.8, 1], [3.7, 3.5], [3.9, 3.9], [3.4, 4.0],[15,15]]
+    X = [[1, 1.1, 1], [1.2, .8, 1.1], [.8, 1, 1.2], [3.7, 3.5, 3.6], [3.9, 3.9, 3.5], [3.4, 3.5, 3.7],[15,15, 15]]
     eps = 0.5
     min_points = 2
     dbscanalgo = dbscan()
-    print dbscanalgo.dbscan(m, eps, min_points)
-    assert dbscanalgo.dbscan(m, eps, min_points) == [0, 0, 0, 1, 1, 1, NOISE]
+    dbscanalgo.dbscan(X, eps, min_points)
+
+    X = StandardScaler().fit_transform(X)
+    db = DBSCAN(eps=eps, min_samples=min_points).fit(X)
+    core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+    core_samples_mask[db.core_sample_indices_] = True
+
+    labels = db.labels_
+    unique_labels = set(labels)
+    colors = plt.cm.Spectral(np.linspace(0, 1, len(unique_labels)))
+    for k, col in zip(unique_labels, colors):
+        if k == -1:
+            # Black used for noise.
+            col = 'k'
+
+        class_member_mask = (labels == k)
+        xy = X[class_member_mask & core_samples_mask]
+        print xy
+        print " "
+        print xy[:, 0], xy[:, 1]
+        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=col,
+                 markeredgecolor='k', markersize=14)
+
+        xy = X[class_member_mask & ~core_samples_mask]
+        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=col,
+                 markeredgecolor='k', markersize=6)
+    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+
+    plt.title('Estimated number of clusters: %d' % n_clusters_)
+    plt.show()
+
 test_dbscan()
 
